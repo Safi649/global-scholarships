aa2ddf5370fd4721f40805b5bf8b2873870ebf2c
Safi649 <muhammadabbassafi332@gmail.com>
2025-07-31 11:48:39 +0500

    Update submit.js

--- pages/submit.js
+++ pages/submit.js
@@ -1,1 +1,18 @@
+import Navbar from '../components/Navbar';
+import Head from 'next/head';
+
+export default function Submit() {
+  return (
+    <>
+      <Head>
+        <title>Submit – Global Scholarships</title>
+      </Head>
+      <Navbar />
+      <main className="p-6">
+        <h1 className="text-3xl font-bold mb-4">Submit a Scholarship</h1>
+        <p>If you know about a scholarship opportunity, please submit the details below (form coming soon).</p>
+      </main>
+    </>
+  );
+}
 
