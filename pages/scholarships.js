76d763c47f7ec0af7177224de74d9874faf19ce0
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-02 15:16:22 +0500

    Update scholarships.js

--- pages/scholarships.js
+++ pages/scholarships.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 
