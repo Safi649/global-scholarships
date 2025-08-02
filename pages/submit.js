a8ffbf578b2c99bd56bd96599951c3a113758510
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-02 17:23:26 +0500

    Update submit.js

--- pages/submit.js
+++ pages/submit.js
@@ -1,4 +1,4 @@
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 import Head from 'next/head';
 import Footer from '../components/footer';
 export default function Submit() {
