832c739fc0e27e1bb301aed861a21ad2569a3bab
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-04 14:30:55 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+// 📁 pages/_app.js
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Global Scholarships</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
