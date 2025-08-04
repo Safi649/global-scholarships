dd4dea35ae06f97bc562cdb48385daed2a37aa45
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-04 10:47:46 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['your-image-domains.com'], // optional
+    domains: ['your-image-domains.com'], // optional: use your actual image domain
   },
 };
 
