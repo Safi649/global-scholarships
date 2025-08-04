68c7ea43c6f1a9dadc6c755633e31a15057108b0
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-04 12:25:49 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyClpsjlFZrsFEi-tVIjoN-R5Wk801gb0sM",
   authDomain: "scholarships-1fc74.firebaseapp.com",
   projectId: "scholarships-1fc74",
-  storageBucket: "scholarships-1fc74.firebasestorage.app",
+  storageBucket: "scholarships-1fc74.appspot.com", // 🔁 corrected domain
   messagingSenderId: "230006779571",
   appId: "1:230006779571:web:04a02a9fadec274e6f4ac6",
   measurementId: "G-1HKY81QFTD",
