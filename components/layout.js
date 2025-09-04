3aacf2cc1ecaa1a8cecb82952c60bc541cc90250
Safi649 <muhammadabbassafi332@gmail.com>
2025-09-04 03:14:35 -0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,15 @@
-// components/layout.js
-import React from 'react';
-import Navbar from './navbar';
-import Footer from './footer';
+// 📁 components/layout.js
+import Navbar from "./navbar";
+import Footer from "./footer";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen">{children}</main>
+      {/* add top padding so page content doesn't hide behind sticky navbar */}
+      <main className="pt-20 min-h-[calc(100vh-5rem)]">
+        {children}
+      </main>
       <Footer />
     </>
   );
